c9e0dfd22d4f76a3ff7adf0b17ac39b459aa03cf
Nick930826 <xianyou1993@qq.com>
2021-06-11 10:31:27 +0800

    update

--- config/config.default.js
+++ config/config.default.js
@@ -59,7 +59,7 @@ module.exports = appInfo => {
       // 密码
       password: 'cxy930826',
       // 数据库名
-      database: 'daily_cost',
+      database: 'jue-cost',
     },
     // 是否加载到 app 上，默认开启
     app: true,
